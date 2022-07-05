0a85e0ab908da9c7824fcaf2b054eed32cb873e2
LindaZhou120 <743740084@qq.com>
2022-07-05 16:07:52 +0800

    first commit

---
+++ dataType.rb
@@ -0,0 +1,63 @@
+# a1 = 0
+# a2 = 1_000_000
+# a3 = 0xa
+# puts a1,a2
+# puts a3
+# 
+# f1 = 0.0
+# f2 = 2.1
+# f3 = 1000000.1
+# 
+# puts f3
+# 
+# 
+# puts 2**(1/4)
+# puts 16**(1/4.0)
+# 
+# 
+# puts 'escape using  "\\"';
+# puts 'That\'s right';
+# 
+# 
+# puts "相乘： #{24*60*60}"
+
+
+# name = "Ruby"
+# puts name
+# puts "#{name + ", ok"}"
+
+
+
+ary = ["fred", 10,3.14, "This is a string","last element" ]
+ary.each do |i|
+  puts i
+end
+
+
+hsh = colors = {"red"=>0xf00, "green"=>0x0f0,"blue"=>0x00f}
+hsh.each do |key,value|
+  print key," is ", value,"\n"
+end
+
+
+
+(10..15).each do |n|
+  print n, ' '
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
